3388dfdb8e06cdd11ce0e3c2b1c0433211b064b8
Gabriel <60001504+gabrielpalves@users.noreply.github.com>
2023-10-25 11:13:04 -0300

    plots ext penalty

--- Python Aula 07/plot_descent_penalty.py
+++ Python Aula 07/plot_descent_penalty.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 # from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 from matplotlib import cm
+import sympy as sym
 
 
 def plot_3d_surface(x, f, f_obj, plot_h, plot_g, f_constraints):
@@ -15,7 +15,6 @@ def plot_3d_surface(x, f, f_obj, plot_h, plot_g, f_constraints):
         f_obj (method): objective function
         plot_h (boolean): True or False, for plotting the equality constraints
         plot_g (boolean): True or False, for plotting the inequality constraints
-        
     """
     
     if isinstance(x, list):
@@ -29,13 +28,13 @@ def plot_3d_surface(x, f, f_obj, plot_h, plot_g, f_constraints):
         
     if len(f.shape) == 2:
         f = f[0, :]
-    
-    print(f.shape, x.shape)
-    
+        
     fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={"projection": "3d"})
     # ax = Axes3D(fig)
-
-    x1 = np.arange(-20, 20, 0.25)
+    
+    xmin = np.floor(-np.max(np.abs(x[0, :])) - 1)
+    xmax = np.ceil(np.max(np.abs(x[0, :])) + 1)
+    x1 = np.arange( xmin, xmax, 0.25 )
     x2 = np.copy(x1)
     X1, X2 = np.meshgrid(x1, x2)
         
@@ -44,6 +43,10 @@ def plot_3d_surface(x, f, f_obj, plot_h, plot_g, f_constraints):
         for j in range(X2.shape[0]):
             f_grid = f_obj([X1[i,j], X2[i,j]])
             y[i,j] = f_grid[0]
+    
+    # To use in the constraints plot
+    ymax = np.max(y)
+    ymin = np.min(y)
 
     surface = ax.plot_surface(X1, X2, y,
                           cmap=cm.coolwarm,
@@ -64,49 +67,68 @@ def plot_3d_surface(x, f, f_obj, plot_h, plot_g, f_constraints):
     # Ending
     ax.plot([x[-1, 0]], [x[-1, 1]], [f[-1]] , markerfacecolor='g', markeredgecolor='g', marker='X', markersize=12)
     
+    def plot_constraints(equality=True):
+        f_grid = f_constraints(np.array([0, 0]))
+        
+        if equality:
+            x2 = f_grid[0]
+            cor = 'g'
+            label = 'Equality constraint '
+        else:
+            x2 = f_grid[2]
+            cor = 'y'
+            label = 'Inequality constraint '
+            
+        n_constraints = x2.size
+        
+        for constraint in range(n_constraints):
+            xx1 = np.array([])
+            xx2 = np.array([])
+            yy = np.array([])
+            x2s = sym.Symbol('x2s')
+            for i in range(x1.shape[0]):
+                f_grid = f_constraints(np.array([x1[i], x2s]))
+                
+                # get the value of x2
+                if equality:
+                    x2 = f_grid[0]
+                else:
+                    x2 = f_grid[2]
+                
+                if n_constraints > 1:
+                    x2 = x2[constraint]
+                
+                # for all results of x2, associate x1 and y values
+                x2 = sym.solve(x2, x2s)
+                for j in range(len(x2)):
+                    x2_value = float(x2[j])
+                    xx1 = np.append(xx1, x1[i])
+                    xx2 = np.append(xx2, x2_value)
+                    yy = np.append(yy, f_obj([x1[i], x2_value])[0])
+            
+            idx = np.argsort(xx2)
+            xx1 = xx1[idx]
+            xx2 = xx2[idx]
+            yy = yy[idx]
+            
+            h, = ax.plot(xx1, xx2, yy, color=cor, label=label+str(constraint+1))
+            ax.legend(handles=[h])
+
     # Plot equality constraints
-    # OU EU USO SYMBOLIC PARA RESOLVER RESTRIÇÕES DE SEGUNDO GRAU OU MAIS (VER CUSTO)
-    # OU SÓ PROGRAMO PARA RESTRIÇÕES DE PRIMEIRO GRAU
-    # TAMBÉM DA PARA PLOTAR UM HIPERPLANO OU A INTERSEÇÃO NA FUNÇÃO OBJETIVO
     if plot_h and f_constraints([X1[0,0], X2[0,0]])[0].size > 0:
-        for i in range(x1.shape[0]):
-            f_grid = f_constraints([x1[i], 0])
-            x2 = -f_grid[0] # get the value of x2
-            f_obj([x1[i]])
+        plot_constraints(equality=True)
     
     # Plot inequality constraints
+    if plot_g and f_constraints([X1[0,0], X2[0,0]])[2].size > 0:
+        plot_constraints(equality=False)
     
-    
-    # if plot_h and f_constraints([X1[0,0], X2[0,0]])[0].size > 0:
-    #     y_h = np.zeros((X1.shape))
-    #     for i in range(X1.shape[0]):
-    #         for j in range(X2.shape[0]):
-    #             f_grid = f_constraints([X1[i,j], X2[i,j]])
-    #             y_h[i,j] = f_grid[0]
-        
-    #     surface = ax.plot_surface(X1, X2, y_h,
-    #                       cmap=cm.PiYG,
-    #                       rstride=1, cstride=1, alpha=0.6)
-        
-    #     fig.colorbar(surface, shrink=0.5, aspect=5)
-    
-    # if plot_g and f_constraints([X1[0,0], X2[0,0]])[2].size > 0:
-    #     y_g = np.zeros((X1.shape))
-    #     for i in range(X1.shape[0]):
-    #         for j in range(X2.shape[0]):
-    #             f_grid = f_constraints([X1[i,j], X2[i,j]])
-    #             y_g[i,j] = f_grid[2]
-        
-    #     surface = ax.plot_surface(X1, X2, y_h,
-    #                       cmap=cm.BrBG,
-    #                       rstride=1, cstride=1, alpha=0.6)
-        
-    #     fig.colorbar(surface, shrink=0.5, aspect=5)
+    ax.set_xlim((xmin, xmax))
+    ax.set_ylim((xmin, xmax))
     
     plt.show()
 
 
-def plot_2d_contour(x, f_obj):
+def plot_2d_contour(x, f_obj, plot_h, plot_g, f_constraints):
     """
     Generates a contour plot, showing the direction of the optimization
     
@@ -123,17 +145,19 @@ def plot_2d_contour(x, f_obj):
     
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 8))
 
-    x1 = np.arange(-20, 20, 0.25)
+    xmin = np.floor(-np.max(np.abs(x[0, :])) - 1)
+    xmax = np.ceil(np.max(np.abs(x[0, :])) + 1)
+    x1 = np.arange( xmin, xmax, 0.25 )
     x2 = np.copy(x1)
-    x1, x2 = np.meshgrid(x1, x2)
+    X1, X2 = np.meshgrid(x1, x2)
     
-    y = np.zeros((x1.shape))
-    for i in range(x1.shape[0]):
-        for j in range(x2.shape[0]):
-            f_grid = f_obj([x1[i,j], x2[i,j]])
+    y = np.zeros((X1.shape))
+    for i in range(X1.shape[0]):
+        for j in range(X2.shape[0]):
+            f_grid = f_obj([X1[i,j], X2[i,j]])
             y[i,j] = f_grid[0]
 
-    contours = ax.contour(x1, x2, y, 30)
+    contours = ax.contour(X1, X2, y, 30)
     ax.clabel(contours)
 
     colors = ['k']
@@ -147,5 +171,63 @@ def plot_2d_contour(x, f_obj):
     ax.set_xlabel(r'$x_1$', labelpad=20, fontsize=24, fontweight='bold')
     ax.set_ylabel(r'$x_2$', labelpad=20, fontsize=24, fontweight='bold')
     ax.set_title('Cost function')
-
+    
+    def plot_constraints(equality=True):
+        f_grid = f_constraints(np.array([0, 0]))
+        
+        if equality:
+            x2 = f_grid[0]
+            cor = 'g'
+            label = 'Equality constraint '
+        else:
+            x2 = f_grid[2]
+            cor = 'y'
+            label = 'Inequality constraint '
+            
+        n_constraints = x2.size
+        
+        for constraint in range(n_constraints):
+            xx1 = np.array([])
+            xx2 = np.array([])
+            yy = np.array([])
+            x2s = sym.Symbol('x2s')
+            for i in range(x1.shape[0]):
+                f_grid = f_constraints(np.array([x1[i], x2s]))
+                
+                # get the value of x2
+                if equality:
+                    x2 = f_grid[0]
+                else:
+                    x2 = f_grid[2]
+                
+                if n_constraints > 1:
+                    x2 = x2[constraint]
+                
+                # for all results of x2, associate x1 and y values
+                x2 = sym.solve(x2, x2s)
+                for j in range(len(x2)):
+                    x2_value = float(x2[j])
+                    xx1 = np.append(xx1, x1[i])
+                    xx2 = np.append(xx2, x2_value)
+                    yy = np.append(yy, f_obj([x1[i], x2_value])[0])
+            
+            idx = np.argsort(xx2)
+            xx1 = xx1[idx]
+            xx2 = xx2[idx]
+            yy = yy[idx]
+            
+            h, = ax.plot(xx1, xx2, color=cor, label=label+str(constraint+1))
+            ax.legend(handles=[h])
+            
+    # Plot equality constraints
+    if plot_h and f_constraints([X1[0,0], X2[0,0]])[0].size > 0:
+        plot_constraints(equality=True)
+    
+    # Plot inequality constraints
+    if plot_g and f_constraints([X1[0,0], X2[0,0]])[2].size > 0:
+        plot_constraints(equality=False)
+    
+    ax.set_xlim((xmin, xmax))
+    ax.set_ylim((xmin, xmax))
+    
     plt.show()
--- Python Aula 07/test.py
+++ Python Aula 07/test.py
@@ -1,39 +1,132 @@
 import numpy as np
+x = np.array([2, 3])
+b = x[0] + x[1]
+a = np.array([1, 1])
+c = np.array([])
 
-x1 = np.arange(-20,20,1)
-print(x1.shape)
+print(b.shape, a.shape, b.size, a.size, c.shape, c.size)
 
-x = np.array([1, -1])
-h = x[0]+x[1]-4
-dh = np.array([1, 1])
-g = np.array([])
-f = np.array([[1,2,3,4],[3,4,5,6]])
+# x1 = np.arange(-20,20,1)
+# print(x1.shape)
 
-print(x.shape, h.shape, dh.shape, f.shape)
-print(x.size, h.size, dh.size, f.size)
+# x = np.array([1, -1])
+# h = x[0]+x[1]-4
+# dh = np.array([1, 1])
+# g = np.array([])
+# f = np.array([[1,2,3,4],[3,4,5,6]])
 
-if h.size == 0:
-    print('not even h')
+# print(x.shape, h.shape, dh.shape, f.shape)
+# print(x.size, h.size, dh.size, f.size)
 
-if g.size == 0:
-    print('not even g')
+# if h.size == 0:
+#     print('not even h')
 
-if not h.shape:
-    print('not')
-else:
-    print('ok')
+# if g.size == 0:
+#     print('not even g')
 
-if not x.shape:
-    print('not x')
+# if not h.shape:
+#     print('not')
+# else:
+#     print('ok')
+
+# if not x.shape:
+#     print('not x')
     
-if len(h.shape) == 0:
-    print('length is zero')
+# if len(h.shape) == 0:
+#     print('length is zero')
 
+# if isinstance(x, list):
+#     x = np.array(x)
 
-if isinstance(x, list):
-    x = np.array(x)
-
-h = x[0]+x[1]-4
-dh = np.array([1, 1])
+def nlconstraints(x):
+    h = x[0]**2+x[1]-4
+    dh = np.array([1, 1])
+    
+    return h, dh
 
 # divide o resultado de 'h' pelo negativo da derivada
+
+# testing symbolic math
+import sympy as sym
+
+abc = sym.Symbol('abc')
+bcd = sym.Symbol('bcd')
+
+a, _ = nlconstraints([abc, 1])
+
+print(a)
+
+result = sym.solve(a, abc)
+print(result, type(result))
+print(result[0], len(result))
+print(float(result[0]))
+
+#
+#
+#
+problem = 2
+def f_obj(x):
+    if problem==1:
+        f = x[0]**2+10*x[1]**2
+        df = np.array([2*x[0], 20*x[1]])
+    elif problem==2:
+        f=(x[0]-1.5)**2+(x[1]-1.5)**2
+        df=np.array([2*(x[0]-1.5), 2*(x[1]-1.5)])
+    elif problem==3:
+        f=(x[0]-1.5)**2+(x[1]-1.5)**2
+        df=np.array([2*(x[0]-1.5), 2*(x[1]-1.5)])
+    elif problem==4:
+        f=(x[0]-1)**2+(x[1]-1)**2
+        df=np.array([2*(x[0]-1), 2*(x[1]-1)])
+    elif problem==5:
+        L=5 # meters
+        f=x[0]*x[1]*L
+        df=np.array([(x[1]*L), (x[0]*L)])   
+        
+    return f, df
+
+x = np.array([[3,3],[2,2]])
+xmin = np.floor(-np.max(np.abs(x[0, :])) - 1)
+xmax = np.ceil(np.max(np.abs(x[0, :])) + 1)
+x1 = np.arange( xmin, xmax, 0.25 )
+x2 = np.copy(x1)
+X1, X2 = np.meshgrid(x1, x2)
+    
+y = np.zeros((X1.shape))
+for i in range(X1.shape[0]):
+    for j in range(X2.shape[0]):
+        f_grid = f_obj([X1[i,j], X2[i,j]])
+        y[i,j] = f_grid[0]
+
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={"projection": "3d"})
+
+surface = ax.plot_surface(X1, X2, y,
+                          cmap=cm.coolwarm,
+                          rstride=1, cstride=1, alpha=0.6)
+
+xx = np.array([-4, -4, -3.5, -3.5, -3, -3, -2.5, -2.5, -2, -2, -1.5, -1.5, -1, -1, -0.5, -0.5, 0, 0, 0.5, 0.5, 1, 1, 1.5, 1.5, 2, 2, 2.5, 2.5, 3, 3, 3.5, 3.5, 4, 4])
+xy = np.array([])
+yy = np.array([])
+for i in range(0, xx.shape[0], 2):
+    r = np.sqrt(-xx[i]+4)
+    # here, xy (x2) is being found
+    # so we need to plot following x2 (xy) order, not x1 (xx)
+    # hence, the sort after the loop
+    f_grid_1 = f_obj([xx[i], -r])
+    f_grid_2 = f_obj([xx[i],  r])
+    
+    xy = np.append(xy, np.array([-r, r]))
+    yy = np.append(yy, np.array([f_grid_1[0], f_grid_2[0]]))
+
+idx = np.argsort(xy)
+xy = xy[idx]
+xx = xx[idx]
+yy = yy[idx]
+
+ax.plot(xx, xy, yy, markerfacecolor='k', markeredgecolor='k', marker='.', markersize=7)
+
+XX, XY = np.meshgrid(xx, xy)
